7779ff3b6f9c029bc5536c4399fad4cb55af0063
demeiz <medievalduel@gmail.com>
2015-05-20 11:42:17 +0100

    Update Neopets_-_Turdle_Betting_Auto.user.js

--- Neopets_-_Turdle_Betting_Auto.user.js
+++ Neopets_-_Turdle_Betting_Auto.user.js
@@ -9,6 +9,8 @@
 // @language    en
 // @include     http://www.neopets.com/medieval/turdlebetting.phtml
 // @require	    http://code.jquery.com/jquery-latest.min.js
+// @downloadURL https://github.com/demeiz/Turdle-Betting/raw/master/Neopets_-_Turdle_Betting_Auto.user.js
+// @updateURL   https://github.com/demeiz/Turdle-Betting/raw/master/Neopets_-_Turdle_Betting_Auto.meta.js
 // @grant       none
 // ==/UserScript==
 
