c4aa3628aff3539b9cbb85c405c18f33798ba9d2
demeiz <medievalduel@gmail.com>
2015-05-20 11:42:37 +0100

    Create Neopets_-_Turdle_Betting_Auto.meta.js

---
+++ Neopets_-_Turdle_Betting_Auto.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Neopets - Turdle Betting Auto
+// @namespace   Jarofgrease.captainmaxthecat.com
+// @author      demeiz
+// @email       admin@captainmaxthecat.com
+// @license     GNU GPL
+// @homepage    http://jarofgrease.captainmaxthecat.com
+// @version     1.0
+// @language    en
+// @include     http://www.neopets.com/medieval/turdlebetting.phtml
+// @require	    http://code.jquery.com/jquery-latest.min.js
+// @downloadURL https://github.com/demeiz/Turdle-Betting/raw/master/Neopets_-_Turdle_Betting_Auto.user.js
+// @updateURL   https://github.com/demeiz/Turdle-Betting/raw/master/Neopets_-_Turdle_Betting_Auto.meta.js
+// @grant       none
+// ==/UserScript==
